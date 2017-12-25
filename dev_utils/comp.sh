b9fa3affc9ecd41887b4c62ce99146e885e2ad33
Konstantin <onefivefournine@gmail.com>
2017-12-25 14:52:42 +0300

    fix

--- dev_utils/comp.sh
+++ dev_utils/comp.sh
@@ -4,6 +4,7 @@ if [ ! -d "$1/$2" ]; then
     mkdir $1/$2
 fi
 
-echo $(cat ./_component-template.vue) > $1/$2/index.vue
+echo "$(cat $(dirname $0)'/_component-template.vue')" > $1/$2/index.vue
+echo "<div></div>" > $1/$2/index.html
 
 echo 'created component '$2' at '$1
